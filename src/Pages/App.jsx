e1b9fa417f9b593168d33aba13b6f6faba7edb50
asugan <cagatayeren1898@gmail.com>
2022-09-14 23:59:39 +0300

    navbar

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "../Styles/App.scss";
+import Navbar from "../Components/Base/Navbar";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <Navbar />;
 }
 
 export default App;
